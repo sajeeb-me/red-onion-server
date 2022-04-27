75ea67aab7b708750d2f84a29669d8c589cbf5aa
sdshuvo98 <hellosdshuvo@gmail.com>
2022-04-27 17:40:35 +0600

    'env'

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ function verifyJWT(req, res, next) {
     })
 }
 
-const uri = `mongodb+srv://redonionAdmin:uB3k0rTZ9O6wV3u5@cluster0.clafk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.clafk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 async function run() {
     try {
